a340b71a1bd99f224d1b06829b1d24ffddc66955
Bill Adams <vikingbill@gmail.com>
2022-03-17 13:24:37 -0400

    add install script

---
+++ src/install.sh
@@ -0,0 +1,24 @@
+{
+    add_psql(){
+        psql -t -A -c "CREATE USER AAINSTRUCTOR WITH PASSWORD 'password' CREATEDB LOGIN;"
+        psql -t -A -c "CREATE DATABASE AASTUDENT WITH OWNER AAINSTRUCTOR"
+    }
+
+    add_function(){
+        echo "start"
+        local profile;
+        if [ "${SHELL}" == "/bin/bash" ]; then
+            profile='.bashrc'
+        elif [ "${SHELL}" == "/bin/zsh" ]; then
+            profile='.zshrc'
+        else
+            echo "Unsure of shell, exiting script"
+            exit 1
+        fi
+        # echo $profile
+        echo | >> ${HOME}/$profile
+        curl -sS https://raw.githubusercontent.com/edenspring/project-gitr/main/src/function >> ${HOME}/$profile
+    }
+    add_psql
+    add_function    
+}
